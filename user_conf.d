5605f52bd63d6654dab24da8ad22b91d41884a10
David Jay <davidgljay@gmail.com>
2022-10-22 23:09:02 -0700

    Switching servers to port 443.

--- user_conf.d
+++ user_conf.d
@@ -1,8 +1,27 @@
-server {
-    listen       80;
-    server_name  analytics.relationalitylab.org;
 
-    #access_log  /var/log/nginx/host.access.log  main;
+server {
+    # Listen to port 443 on both IPv4 and IPv6.
+    listen 443 ssl default_server reuseport;
+    listen [::]:443 ssl default_server reuseport;
+
+    # Domain names this server should respond to.
+    server_name analytics.relationalitylab.org;
+
+    # Load the ECDSA certificates.
+    ssl_certificate     /etc/letsencrypt/live/test-ecc/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/test-ecc/privkey.pem;
+
+    # Load the RSA certificates.
+    ssl_certificate     /etc/letsencrypt/live/test-rsa/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/test-rsa/privkey.pem;
+
+    # Load the Diffie-Hellman parameter.
+    ssl_dhparam /etc/letsencrypt/dhparams/dhparam.pem;
+
+    # Define the ciphers to use in the preferred order.
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_prefer_server_ciphers on;
+    ssl_ciphers "EECDH+ECDSA+AESGCM:EECDH+aRSA+AESGCM:EECDH+ECDSA+SHA384:EECDH+ECDSA+SHA256:EECDH+aRSA+SHA384:EECDH+aRSA+SHA256:EECDH:DHE+AESGCM:DHE:!RSA!aNULL:!eNULL:!LOW:!RC4:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS:!CAMELLIA:!SEED";
 
     location / {
         proxy_pass http://172.18.0.2;
@@ -17,37 +36,34 @@ server {
         root   /usr/share/nginx/html;
     }
 
-    # proxy the PHP scripts to Apache listening on 127.0.0.1:80
-    #
-    #location ~ \.php$ {
-    #    proxy_pass   http://127.0.0.1;
-    #}
-
-    # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
-    #
-    #location ~ \.php$ {
-    #    root           html;
-    #    fastcgi_pass   127.0.0.1:9000;
-    #    fastcgi_index  index.php;
-    #    fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
-    #    include        fastcgi_params;
-    #}
-
-    # deny access to .htaccess files, if Apache's document root
-    # concurs with nginx's one
-    #
-    #location ~ /\.ht {
-    #    deny  all;
-    #}
 }
 
 server {
-   listen 80;
-   server_name docs.relationalitylab.org;
+    # Listen to port 443 on both IPv4 and IPv6.
+    listen 443 ssl default_server reuseport;
+    listen [::]:443 ssl default_server reuseport;
 
-   location / {
+    # Load the ECDSA certificates.
+    ssl_certificate     /etc/letsencrypt/live/test-ecc/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/test-ecc/privkey.pem;
+
+    # Load the RSA certificates.
+    ssl_certificate     /etc/letsencrypt/live/test-rsa/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/test-rsa/privkey.pem;
+
+    # Load the Diffie-Hellman parameter.
+    ssl_dhparam /etc/letsencrypt/dhparams/dhparam.pem;
+
+    # Define the ciphers to use in the preferred order.
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_prefer_server_ciphers on;
+    ssl_ciphers "EECDH+ECDSA+AESGCM:EECDH+aRSA+AESGCM:EECDH+ECDSA+SHA384:EECDH+ECDSA+SHA256:EECDH+aRSA+SHA384:EECDH+aRSA+SHA256:EECDH:DHE+AESGCM:DHE:!RSA!aNULL:!eNULL:!LOW:!RC4:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS:!CAMELLIA:!SEED";
+
+    server_name docs.relationalitylab.org;
+
+    location / {
      proxy_pass http://172.18.0.5;
-   }
+    }
 
     error_page  404              /404.html;
 
